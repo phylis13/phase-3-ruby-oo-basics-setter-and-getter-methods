2c30b6921f9f8a2648f55c100390dec80c30a541
phylis13 <phylisnjeri54@gmail.com>
2023-02-09 23:26:21 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
+    attr_reader :name
+    def name= (name)
+        @name = name
+    end 
 
+    attr_reader :breed
+    def breed= (breed)
+        @breed = breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
-
-end
+    attr_reader :name
+       def name= (name)
+           @name = name
+       end 
+   
+       attr_reader :job
+       def job= (job)
+           @job = job
+       end
+   end
+   
